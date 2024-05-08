32daa47c55ff5f97384570033e2473b3b3b0d35e
tarifalhasan <tarifalhasan999@gmail.com>
2024-05-08 13:11:53 +0600

    create expo router

---
+++ app/(app)/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="flex-1 bg-indigo-500">
+      <Text>Home</Text>
+    </View>
+  );
+};
+
+export default Home;
